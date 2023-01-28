545eb2bdf1f3267b6402056eb20d08f831d4b51d
tagir gilmutdinov <tagiroid@gmail.com>
2023-01-28 23:28:48 +0500

    minor changes

--- Part 11 testing/test_name_function.py
+++ Part 11 testing/test_name_function.py
@@ -4,8 +4,9 @@ from name_function import get_formatted_name
 
 class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
-        formated = get_formatted_name('janis', 'joplin')
-        self.assertEqual(formated, 'Janis Joplin')
+        formatted = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted, 'Janis Joplin')
+
 
 if __name__ == '__main__':
     unittest.main()
